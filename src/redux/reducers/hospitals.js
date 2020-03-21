589320050bf0aa045139dcf268d3546d2b657ded
Sebastian Dähnrich <sdaehnrich@ebay.com>
2020-03-21 17:07:56 +0100

    initalstate

--- src/redux/reducers/hospitals.js
+++ src/redux/reducers/hospitals.js
@@ -1,4 +1,33 @@
-const initialState = [{ name: "Krankenhaus 1" }, { name: "Krankenhaus 2" }, { name: "Krankenhaus 3" }];
+const initialState = [
+    {
+        hospitals: [
+            {
+                name: "NAME",
+                tel: "030 765765756",
+
+                contact: {
+                    name: "Vorname",
+                    surname: "Nachname",
+                    tel: "030 42354463534"
+                },
+                adress: {
+                    cordinates: {
+                        lang: "432423.3",
+                        lat: "534534534"
+                    },
+                    street: "Straße 67",
+                    postalcode: 13467
+                },
+                hilfsmittel: [
+                    {
+                        name: "Name",
+                        status: 0
+                    }
+                ]
+            }
+        ]
+    }
+];
 
 export default function(state = initialState, action) {
     switch (action.type) {
