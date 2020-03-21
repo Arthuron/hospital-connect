e0d5d50208b3c82a9d62d7474fd98d22a77bb7e0
Jan Schneider <schjan@orkflo.ws>
2020-03-21 14:17:51 +0100

    add card info

--- src/components/adressCard/AdressCard.js
+++ src/components/adressCard/AdressCard.js
@@ -2,6 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 import Card from "../card/Card";
 
+
+// vlt nutxzen wir besser die card cmp aus dem material- ui
+// https://material-ui.com/de/components/cards/
+
 const AdressCard = ({}) => {
     return <Card>ADRESSE</Card>;
 };
--- src/pages/recordstock/Recordstock.js
+++ src/pages/recordstock/Recordstock.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Button } from '@material-ui/core';
+import {Button} from '@material-ui/core';
 
 export default function Recordstock() {
     return (
         <div>
             <h2>Record Stock</h2>
-            <Button color="primary" variant="contained" >Button</Button>
+            <Button color="primary" variant="contained">Button</Button>
         </div>
     );
 }
