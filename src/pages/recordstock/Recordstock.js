3d1ea881bfccb47bb1ac70c661578ee623594f2d
Jan Schneider <schjan@orkflo.ws>
2020-03-21 21:46:38 +0100

    add clickflow for stock

--- src/pages/recordstock/Recordstock.js
+++ src/pages/recordstock/Recordstock.js
@@ -1,23 +1,61 @@
 import React from "react";
 import Container from '@material-ui/core/Container';
 import ChooseKindOfRequest from "./ChooseKindOfRequest";
+import AskForWhichNeed from "./AskForWhichNeed";
+import Editstock from "../editstock/Editstock";
 
 const Recordstock = ({hospitals}) => {
+
+    const [clickFlowStatus, setclickFlowStatus] = React.useState(0);
 
     // fake ID - muss spaeter durch einen login bekannt sein.
     const userID = '42';
     const hospital = hospitals[userID];
     console.log(hospital);
 
+    // 0 = init
+    // 1 = HR request
+    // 2 = item request
+    // 3 = item list
+
+    let _incrementFlowStatus = () => {
+        setclickFlowStatus(clickFlowStatus + 1);
+    };
+    let _decrementFlowStatus = () => {
+        setclickFlowStatus(clickFlowStatus - 1);
+    };
+
     return (
         <Container fixed>
-            <ChooseKindOfRequest name={hospital.name}/>
+            {clickFlowStatus === 0 &&
+            <ChooseKindOfRequest
+                name={hospital.name}
+                need={() => _incrementFlowStatus()}
+                offer={''}
+            />}
+            {clickFlowStatus === 1 &&
+            <AskForWhichNeed
+                name={hospital.name}
+                needType={'Personal'}
+                need={() => _decrementFlowStatus()}
+                notNeed={() => _incrementFlowStatus()}
+            />}
+            {clickFlowStatus === 2 &&
+            <AskForWhichNeed
+                name={hospital.name}
+                needType={'Hilfsmittel'}
+                need={() => _incrementFlowStatus()}
+                notNeed={() => _decrementFlowStatus()}
+            />}
+            {clickFlowStatus === 3 &&
+            <Editstock
+                name={hospital.name}
+            />}
         </Container>
     );
 };
 
 Recordstock.propTypes = {};
-
 Recordstock.defaultProps = {};
 
 export default Recordstock;
